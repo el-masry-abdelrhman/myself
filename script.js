ea2e9f9998035ba50e4d03c2e6f704fb35b522f7
el-masry-abdelrhman <167316924+el-masry-abdelrhman@users.noreply.github.com>
2024-04-26 00:06:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollDown() {
+    // Récupérer la hauteur de la fenêtre
+    const windowHeight = window.innerHeight;
+    
+    // Faire défiler vers le bas d'une partie de la fenêtre (par exemple, la moitié de la hauteur de la fenêtre)
+    window.scrollBy(0, windowHeight / 2);
+  }
