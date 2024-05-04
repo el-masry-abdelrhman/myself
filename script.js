bd6a4598f969a7faf50b75e4f4ed5d8d858c1544
Abdelrhman El Masry <167316924+el-masry-abdelrhman@users.noreply.github.com>
2024-05-04 13:40:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ function scrollDown() {
     // Faire défiler vers le bas d'une partie de la fenêtre (par exemple, la moitié de la hauteur de la fenêtre)
     window.scrollBy(0, windowHeight / 2);
   }
+
+  function changerLangue(langue) {
+    var elementsFr = document.querySelectorAll('.fr');
+    var elementsEn = document.querySelectorAll('.en');
+    var elements_titre_fr = document.querySelectorAll('.titre_fr');
+    var elements_titre_en = document.querySelectorAll('.titre_en');
+
+    if (langue === 'fr') {
+        elementsFr.forEach(function(element) {
+            element.style.display = 'block';
+        });
+        elementsEn.forEach(function(element) {
+            element.style.display = 'none';
+        });
+        elements_titre_fr.forEach(function(element) {
+            element.style.display = 'block';
+        });
+        elements_titre_en.forEach(function(element) {
+            element.style.display = 'none';
+        });
+    } else if (langue === 'en') {
+        elementsFr.forEach(function(element) {
+            element.style.display = 'none';
+        });
+        elementsEn.forEach(function(element) {
+            element.style.display = 'block';
+        });
+        elements_titre_fr.forEach(function(element) {
+            element.style.display = 'none';
+        });
+        elements_titre_en.forEach(function(element) {
+            element.style.display = 'block';
+        });
+    }
+}
